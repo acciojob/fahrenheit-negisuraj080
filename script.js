cd78a206cbf65553c9746194cc185d5fbaac62ce
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-31 10:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	if (typeof celsius !== 'number' || isNaN(celsius)) {
+        return 'Invalid input. Please provide a valid number.';
+    }
+
+    // Convert Celsius to Fahrenheit using the formula: (Celsius * 9/5) + 32
+    const fahrenheit = (celsius * 9/5) + 32;
+
+    // Return the result with two decimal points of precision
+    return fahrenheit.toFixed(2);
 }
 
 // Do not change the code below
